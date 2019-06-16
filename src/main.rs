5bb13c456cd0bd46dc92409cdbddc00c6db4ebef
nyrox <mark.oliver@hotmail.de>
2019-06-16 21:56:11 +0200

    Fix & Fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 extern crate cpal;
 
 use std::sync::Arc;
@@ -7,14 +6,12 @@ use std::sync::Mutex;
 mod synth;
 use synth::nodes::*;
 
-use midir::{MidiInput, Ignore};
-
 fn main() {
-    
-
     //Setup cpal
     let device = cpal::default_output_device().expect("Failed to get default output device");
-    let format = device.default_output_format().expect("Failed to get default output format");
+    let format = device
+        .default_output_format()
+        .expect("Failed to get default output format");
     let event_loop = cpal::EventLoop::new();
     let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
     event_loop.play_stream(stream_id.clone());
@@ -22,11 +19,11 @@ fn main() {
     let sample_rate = format.sample_rate.0 as f32;
     let mut sample_clock = 0f32;
 
-    let tree = MidiInputOscillator::new (WaveType::Sawtooth, 1, 1);
-    
-    let tree = Mutex::new (tree);
-    let tree = Arc::new (tree);
-    
+    let tree = MidiInputOscillator::new(WaveType::Sawtooth, 1, 1);
+
+    let tree = Mutex::new(tree);
+    let tree = Arc::new(tree);
+
     //Get 1 sample recursively
     let mut next_value = (move |out_node: Arc<Mutex<dyn Node>>| {
         move || {
@@ -35,45 +32,49 @@ fn main() {
 
             let context = SamplingContext {
                 clock: sample_position,
-                sample_rate
+                sample_rate,
             };
-                     
-            let out = out_node.lock().unwrap().sample (&context);
-            
+
+            let out = out_node.lock().unwrap().sample(&context);
+
             //println!("{}", out.min(1.0).max(-1.0));
-            
-            out.min(1.0).max(-1.0)   
+
+            out.min(1.0).max(-1.0)
         }
-    }) (tree);
+    })(tree);
 
     //Boilerplate shit
-    event_loop.run(move |_, data| {
-        match data {
-            cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::U16(mut buffer) } => {
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                    for out in sample.iter_mut() {
-                        *out = value;
-                    }
+    event_loop.run(move |_, data| match data {
+        cpal::StreamData::Output {
+            buffer: cpal::UnknownTypeOutputBuffer::U16(mut buffer),
+        } => {
+            for sample in buffer.chunks_mut(format.channels as usize) {
+                let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                for out in sample.iter_mut() {
+                    *out = value;
                 }
-            },
-            cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::I16(mut buffer) } => {
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    let value = (next_value() * std::i16::MAX as f32) as i16;
-                    for out in sample.iter_mut() {
-                        *out = value;
-                    }
-                }
-            },
-            cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    let value = next_value();
-                    for out in sample.iter_mut() {
-                        *out = value;
-                    }
-                }
-            },
-            _ => (),
+            }
         }
+        cpal::StreamData::Output {
+            buffer: cpal::UnknownTypeOutputBuffer::I16(mut buffer),
+        } => {
+            for sample in buffer.chunks_mut(format.channels as usize) {
+                let value = (next_value() * std::i16::MAX as f32) as i16;
+                for out in sample.iter_mut() {
+                    *out = value;
+                }
+            }
+        }
+        cpal::StreamData::Output {
+            buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer),
+        } => {
+            for sample in buffer.chunks_mut(format.channels as usize) {
+                let value = next_value();
+                for out in sample.iter_mut() {
+                    *out = value;
+                }
+            }
+        }
+        _ => (),
     });
 }
--- src/synth/nodes.rs
+++ src/synth/nodes.rs
@@ -1,11 +1,7 @@
-use super::constants::*;
 use super::waves::*;
 
-use std::io::{stdin, stdout, Write};
-use std::error::Error;
-
+use midir::MidiInput;
 use midir::MidiInputConnection;
-use midir::{MidiInput, Ignore};
 
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -17,7 +13,7 @@ pub struct SamplingContext {
 }
 
 pub trait Node: Send {
-    fn sample (&self, ctx: &SamplingContext) -> f32;
+    fn sample(&self, ctx: &SamplingContext) -> f32;
 }
 
 #[derive(Clone, Copy)]
@@ -25,37 +21,35 @@ pub enum WaveType {
     Sine,
     Square,
     Sawtooth,
-    Triangle
+    Triangle,
 }
 
 pub struct WaveGenerator {
     pub freq: f32,
     pub offset: f32,
-    pub wave_type: WaveType
+    pub wave_type: WaveType,
 }
 
 impl Node for WaveGenerator {
-    fn sample (&self, ctx: &SamplingContext) -> f32 {
+    fn sample(&self, ctx: &SamplingContext) -> f32 {
         match self.wave_type {
             WaveType::Sine => sine_wave(self.freq, ctx.clock, self.offset),
             WaveType::Square => square_wave(self.freq, ctx.clock, self.offset),
             WaveType::Sawtooth => sawtooth_wave(self.freq, ctx.clock, self.offset),
-            WaveType::Triangle => triangle_wave(self.freq, ctx.clock, self.offset)
-        }   
+            WaveType::Triangle => triangle_wave(self.freq, ctx.clock, self.offset),
+        }
     }
 }
-
 
 pub struct Sum {
     pub nodes: Vec<Box<dyn Node>>,
 }
 
 impl Node for Sum {
-    fn sample (&self, ctx: &SamplingContext) -> f32 {
-        self.nodes.iter().map (|n| n.sample(ctx)).sum()
+    fn sample(&self, ctx: &SamplingContext) -> f32 {
+        self.nodes.iter().map(|n| n.sample(ctx)).sum()
     }
 }
-
 
 pub struct MidiInputOscillator {
     pub wavetype: WaveType,
@@ -66,23 +60,33 @@ pub struct MidiInputOscillator {
 }
 
 impl Node for MidiInputOscillator {
-    fn sample (&self, ctx: &SamplingContext) -> f32 {
+    fn sample(&self, ctx: &SamplingContext) -> f32 {
         let mut output = 0.0;
 
-        for (pitch, velocity) in self.note_buffer.lock().expect("failed to lock note_buffer").iter().enumerate() {
-           if *velocity == 0 { continue;  }
-            
-           let a4: i8 = 60;
-           let a4_freq = 440.0;
-           let dpitch = pitch as i8 - a4;
+        for (pitch, velocity) in self
+            .note_buffer
+            .lock()
+            .expect("failed to lock note_buffer")
+            .iter()
+            .enumerate()
+        {
+            if *velocity == 0 {
+                continue;
+            }
 
-           let freq = 2.0f32.powf ((dpitch as f32/12.0).into()) * a4_freq;
+            let a4: i8 = 60;
+            let a4_freq = 440.0;
+            let dpitch = pitch as i8 - a4;
 
-           output = output + WaveGenerator {
-               wave_type: self.wavetype,
-               offset: 0.0,
-               freq
-            }.sample (ctx);
+            let freq = 2.0f32.powf((dpitch as f32 / 12.0).into()) * a4_freq;
+
+            output = output
+                + WaveGenerator {
+                    wave_type: self.wavetype,
+                    offset: 0.0,
+                    freq,
+                }
+                .sample(ctx);
         }
 
         output / 5.0
@@ -90,43 +94,51 @@ impl Node for MidiInputOscillator {
 }
 
 impl MidiInputOscillator {
-    pub fn new (wavetype: WaveType, input_device: usize, midi_channel: u8) -> Self {
-        let mut midi_in = MidiInput::new("midir reading input").expect("Failed to create midi input");
-    
+    pub fn new(wavetype: WaveType, input_device: usize, midi_channel: u8) -> Self {
+        let midi_in = MidiInput::new("midir reading input").expect("Failed to create midi input");
+
         let note_buffer = Arc::new(Mutex::new([0; 128]));
-        
-        // 
+
+        //
         let _note_buffer = note_buffer.clone();
-        let _conn_in = midi_in.connect (input_device, "midir-read-input", move |stamp, message, _| {
-            let status = message[0];
+        let _conn_in = midi_in
+            .connect(
+                input_device,
+                "midir-read-input",
+                move |_stamp, message, _| {
+                    let status = message[0];
 
-            let channel = status & 0b00001111;
-            
+                    let _channel = status & 0b00001111;
 
-            let p_type = status & 0b11110000;
+                    let p_type = status & 0b11110000;
 
+                    match p_type {
+                        // NOTE_ ON
+                        0b10010000 => {
+                            let pitch = message[1] & 0b01111111;
+                            let velocity = message[2] & 0b01111111;
 
-            match p_type {
-                // NOTE_ ON
-                0b10010000 => {
-                    let pitch = message [1] & 0b01111111;
-                    let velocity = message [2] & 0b01111111;
-   
-                    println! ("Turning note: {} on to velocity {}", pitch, velocity);
+                            println!("Turning note: {} on to velocity {}", pitch, velocity);
 
-                    _note_buffer.lock().expect("failed to lock note_buffer")[pitch as usize] = velocity;
+                            _note_buffer.lock().expect("failed to lock note_buffer")
+                                [pitch as usize] = velocity;
+                        }
+                        0b10000000 => {
+                            let pitch = message[1] & 0b01111111;
+
+                            println!("Turning note: {} off", pitch);
+                            _note_buffer.lock().expect("failed to lock note_buffer")
+                                [pitch as usize] = 0;
+                        }
+                        _ => {
+                            return;
+                        }
+                    }
                 },
-                0b10000000 => {
-                    let pitch = message [1] & 0b01111111;
+                (),
+            )
+            .expect("failed to create midi listening connection");
 
-                    println! ("Turning note: {} off", pitch);
-                    _note_buffer.lock().expect("failed to lock note_buffer")[pitch as usize] = 0;
-                },
-                _ => { return; } 
-            }
-
-        }, ()).expect("failed to create midi listening connection");
-       
         MidiInputOscillator {
             wavetype,
             input_device,
@@ -136,11 +148,14 @@ impl MidiInputOscillator {
         }
     }
 
-    pub fn reset (&mut self) {
-        for n in self.note_buffer.lock().expect("failed to lock note_buffer").iter_mut() {
+    pub fn reset(&mut self) {
+        for n in self
+            .note_buffer
+            .lock()
+            .expect("failed to lock note_buffer")
+            .iter_mut()
+        {
             *n = 0;
         }
     }
 }
-
-
