47157fecc383bd706766df51f61661a5257890fb
nyrox <mark.oliver@hotmail.de>
2019-06-16 21:55:29 +0200

    Refactor

--- src/synth/nodes.rs
+++ src/synth/nodes.rs
@@ -1,6 +1,15 @@
 use super::constants::*;
 use super::waves::*;
 
+use std::io::{stdin, stdout, Write};
+use std::error::Error;
+
+use midir::MidiInputConnection;
+use midir::{MidiInput, Ignore};
+
+use std::sync::Arc;
+use std::sync::Mutex;
+
 #[derive(Clone, Copy)]
 pub struct SamplingContext {
     pub clock: f32,
@@ -46,3 +55,92 @@ impl Node for Sum {
         self.nodes.iter().map (|n| n.sample(ctx)).sum()
     }
 }
+
+
+pub struct MidiInputOscillator {
+    pub wavetype: WaveType,
+    input_device: usize,
+    midi_channel: u8,
+    note_buffer: Arc<Mutex<[u8; 128]>>,
+    connection: MidiInputConnection<()>,
+}
+
+impl Node for MidiInputOscillator {
+    fn sample (&self, ctx: &SamplingContext) -> f32 {
+        let mut output = 0.0;
+
+        for (pitch, velocity) in self.note_buffer.lock().expect("failed to lock note_buffer").iter().enumerate() {
+           if *velocity == 0 { continue;  }
+            
+           let a4: i8 = 60;
+           let a4_freq = 440.0;
+           let dpitch = pitch as i8 - a4;
+
+           let freq = 2.0f32.powf ((dpitch as f32/12.0).into()) * a4_freq;
+
+           output = output + WaveGenerator {
+               wave_type: self.wavetype,
+               offset: 0.0,
+               freq
+            }.sample (ctx);
+        }
+
+        output / 5.0
+    }
+}
+
+impl MidiInputOscillator {
+    pub fn new (wavetype: WaveType, input_device: usize, midi_channel: u8) -> Self {
+        let mut midi_in = MidiInput::new("midir reading input").expect("Failed to create midi input");
+    
+        let note_buffer = Arc::new(Mutex::new([0; 128]));
+        
+        // 
+        let _note_buffer = note_buffer.clone();
+        let _conn_in = midi_in.connect (input_device, "midir-read-input", move |stamp, message, _| {
+            let status = message[0];
+
+            let channel = status & 0b00001111;
+            
+
+            let p_type = status & 0b11110000;
+
+
+            match p_type {
+                // NOTE_ ON
+                0b10010000 => {
+                    let pitch = message [1] & 0b01111111;
+                    let velocity = message [2] & 0b01111111;
+   
+                    println! ("Turning note: {} on to velocity {}", pitch, velocity);
+
+                    _note_buffer.lock().expect("failed to lock note_buffer")[pitch as usize] = velocity;
+                },
+                0b10000000 => {
+                    let pitch = message [1] & 0b01111111;
+
+                    println! ("Turning note: {} off", pitch);
+                    _note_buffer.lock().expect("failed to lock note_buffer")[pitch as usize] = 0;
+                },
+                _ => { return; } 
+            }
+
+        }, ()).expect("failed to create midi listening connection");
+       
+        MidiInputOscillator {
+            wavetype,
+            input_device,
+            midi_channel,
+            note_buffer: note_buffer.clone(),
+            connection: _conn_in,
+        }
+    }
+
+    pub fn reset (&mut self) {
+        for n in self.note_buffer.lock().expect("failed to lock note_buffer").iter_mut() {
+            *n = 0;
+        }
+    }
+}
+
+
